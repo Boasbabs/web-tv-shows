50d6da0080a9926e2451c77445476bce30801040
Boasbabs <babalolasimeon@gmail.com>
2022-01-09 18:19:58 +0100

    feat(app): add input & table components with styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import { Navbar } from 'components';
+import { Navbar, Table } from 'components';
+import { FiPlusCircle, FiSearch } from 'react-icons/fi';
 
 import './styles/index.scss';
-import styles from './App.module.scss';
+import './App.scss';
 
 function App() {
   return (
-    <div className={styles.App}>
+    <div className="page">
       <Navbar />
-      <button>Button</button>
+      <main className="page__container">
+        {/* Header section */}
+        <section className="page__header">
+          <div className="page__header_wrapper">
+            <div>
+              <h2 className="page__header_title">Category</h2>
+              <p className="page__header_subtitle">Counter 56</p>
+            </div>
+
+            <button className="page__header_button default">
+              <FiPlusCircle />
+              Select Category
+            </button>
+          </div>
+
+          <div className="page__header_form">
+            <div class="input-container fluid ">
+              <FiSearch />
+              <input
+                type="text"
+                className="input-field"
+                placeholder="Search for shows, episodes"
+              />
+            </div>
+          </div>
+        </section>
+
+        {/* Table section */}
+        <section className="table">
+          <Table />
+        </section>
+      </main>
     </div>
   );
 }
