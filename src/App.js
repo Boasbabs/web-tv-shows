98b2e1724e1dc141d1ffead1a13d296241e3f1e1
Boasbabs <babalolasimeon@gmail.com>
2022-01-27 20:31:33 +0100

    chore(CI/CD): add surge deployment workflow

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ function App() {
         <section className="page__header">
           <div className="page__header_wrapper">
             <div>
-              <h2 className="page__header_title">TV Maze Shows</h2>
+              <h2 className="page__header_title">TV Maze Shows (Improved)</h2>
               <p className="page__header_subtitle">Total: {apiData.length}</p>
             </div>
           </div>
